9d6f86c086a8eeec92015b1a7f272c61dede373b
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-02-24 14:25:16 +0800

    display the digital clock

---
+++ exer.js
@@ -0,0 +1,15 @@
+function run() {
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('output').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(run, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
